b609c06f5d7b33d579495ddac16e9f67be1ed0f6
imtiyazdurrotunnasha <codingcumanai@gmail.com>
2025-07-01 19:38:49 +0700

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ function Home() {
     <div className="row align-items-center">
       <div className="col-md-6 text-start">
         <h1 className="display-4 mb-3">Selamat Datang di halaman frondend saya</h1>
-        <p className="lead">Saya adalah <strong>Mahasiswa STMIK WIDYA UTAMA PURWOKERTO</strong> yang fokus pada solusi web modern dan performa tinggi.</p>
+        <p className="lead">Nama Saya Imtiyaz Durrotunnasha. Saya adalah <strong>Mahasiswa STMIK WIDYA UTAMA PURWOKERTO</strong> yang fokus pada solusi web modern dan performa tinggi.</p>
         <a href="#portfolio" className="btn btn-outline-primary mt-3">Lihat Portofolio</a>
       </div>
       <div className="col-md-6 text-center">
