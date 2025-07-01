ebd08e6e5216324492f3c4e5871978587069accf
imtiyazdurrotunnasha <codingcumanai@gmail.com>
2025-07-01 19:37:14 +0700

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,8 +2,8 @@ function Home() {
   return (
     <div className="row align-items-center">
       <div className="col-md-6 text-start">
-        <h1 className="display-4 mb-3">Selamat Datang!</h1>
-        <p className="lead">Saya adalah <strong>Fullstack JavaScript Developer</strong> yang fokus pada solusi web modern dan performa tinggi.</p>
+        <h1 className="display-4 mb-3">Selamat Datang di halaman frondend saya</h1>
+        <p className="lead">Saya adalah <strong>Mahasiswa STMIK WIDYA UTAMA PURWOKERTO</strong> yang fokus pada solusi web modern dan performa tinggi.</p>
         <a href="#portfolio" className="btn btn-outline-primary mt-3">Lihat Portofolio</a>
       </div>
       <div className="col-md-6 text-center">
